7b16a6e44d5fb573388a798abc4759fad7f6b0fa
Mauro Bianco <mbianco@cscs.ch>
2015-12-11 18:57:56 +0100

    refactored traits

--- Code/Traits/traits.cpp
+++ Code/Traits/traits.cpp
@@ -6,17 +6,23 @@
 template <typename ValueType>
 struct A {
     using value_type = ValueType;
+    static constexpr bool has_separators = true;
 
     value_type value;
 
-    A(value_type v) : value(v) {}
+    A(value_type v) : value{v} {}
+
+    static std::string left_separator() {return " (";}
+    static std::string right_separator() {return ") ";}
 };
 
 struct B {
 
+    using value_type = std::string;
     std::string value;
+    static constexpr bool has_separators = false;
 
-    B(std::string && v) : value(std::move(v)) {}
+    B(std::string && v) : value{std::move(v)} {}
 };
 
 /**
@@ -26,18 +32,44 @@ struct B {
 template <typename T>
 struct my_traits {
 
-    template <typename U>
-    static std::string my_to_string(U const& v) { return std::to_string(v); }
-    static std::string my_to_string(std::string const& v) { return v; }
+    static constexpr bool has_separators = T::has_separators;
+    using value_type = typename T::value_type;
 
-    static std::string left_separator() {return " (";}
-    static std::string right_separator() {return ") ";}
-    static constexpr std::string get_value(T const& f)
+
+    static value_type get_value(T const& f)
     {
-        return my_to_string(f.value);
+        return f.value;
     }
+
+    static std::string default_separator() {return " - ";}
 };
 
+
+namespace print_impl {
+    template <typename Traits, typename ObjectType>
+    typename std::enable_if<Traits::has_separators, std::string>::type
+    get_left_separator(ObjectType const& o) {
+        return o.left_separator();
+    }
+
+    template <typename Traits, typename ObjectType>
+    typename std::enable_if<! Traits::has_separators, std::string>::type
+    get_left_separator(ObjectType const&) {
+        return Traits::default_separator();
+    }
+
+    template <typename Traits, typename ObjectType>
+    typename std::enable_if<Traits::has_separators, std::string>::type
+    get_right_separator(ObjectType const& o) {
+        return o.right_separator();
+    }
+
+    template <typename Traits, typename ObjectType>
+    typename std::enable_if<! Traits::has_separators, std::string>::type
+    get_right_separator(ObjectType const&) {
+        return Traits::default_separator();
+    }
+}// namespace print_impl
 
 /**
    The print function will take a list of objects for which the traits are valid
@@ -51,7 +83,11 @@ template <typename First, typename... T>
 void print(First const& f, T const&... v) {
     using traits = my_traits<First>;
 
-    std::cout << traits::left_separator() << traits::get_value(f) << traits::right_separator();
+    auto ls = print_impl::get_left_separator<traits>(f);
+    auto rs = print_impl::get_right_separator<traits>(f);
+    typename traits::value_type value = traits::get_value(f);
+
+    std::cout << ls << value << rs;
     print(v...);
 }
 
--- Code/Traits/traits1.cpp
+++ Code/Traits/traits1.cpp
@@ -1,78 +1,80 @@
 #include <iostream>
 
-
+/**
+   Classes A and B are two user classes that implements a given interface.
+ */
 template <typename ValueType>
 struct A {
     using value_type = ValueType;
+    static constexpr bool has_separators = true;
 
     value_type value;
 
-    A(value_type v) : value(v) {}
+    A(value_type v) : value{v} {}
+
+    static std::string left_separator() {return " (";}
+    static std::string right_separator() {return ") ";}
 };
 
 struct B {
 
+    using value_type = std::string;
     std::string value;
+    static constexpr bool has_separators = false;
 
-    B(std::string && v) : value(std::move(v)) {}
+    B(std::string && v) : value{std::move(v)} {}
 };
 
-
+/**
+   A traits class is a class template that contains informations on how to manipulate
+   objects of the types for which the traits are meaningful.
+ */
 template <typename T>
 struct my_traits {
 
-    template <typename U>
-    static std::string my_to_string(U const& v) { return std::to_string(v); }
-    static std::string my_to_string(std::string const& v) { return v; }
+    static constexpr bool has_separators = T::has_separators;
+    using value_type = typename T::value_type;
 
-    static std::string left_separator() {return " (";}
-    static std::string right_separator() {return ") ";}
-    static std::string get_value(T const& f)
+
+    static value_type get_value(T const& f)
     {
-        return my_to_string(f.value);
+        return f.value;
     }
+
+    static std::string default_separator() {return " - ";}
 };
+
+
+namespace print_impl {
+    template <typename Traits, typename ObjectType>
+    typename std::enable_if<Traits::has_separators, std::string>::type
+    get_left_separator(ObjectType const& o) {
+        return o.left_separator();
+    }
+
+    template <typename Traits, typename ObjectType>
+    typename std::enable_if<! Traits::has_separators, std::string>::type
+    get_left_separator(ObjectType const&) {
+        return Traits::default_separator();
+    }
+
+    template <typename Traits, typename ObjectType>
+    typename std::enable_if<Traits::has_separators, std::string>::type
+    get_right_separator(ObjectType const& o) {
+        return o.right_separator();
+    }
+
+    template <typename Traits, typename ObjectType>
+    typename std::enable_if<! Traits::has_separators, std::string>::type
+    get_right_separator(ObjectType const&) {
+        return Traits::default_separator();
+    }
+}// namespace print_impl
 
 /**
-   This is a third party class thatwe want to use with our
-   print function. We can then define a specialization of the trairs.
+   The print function will take a list of objects for which the traits are valid
+   and print something accorgingly.
  */
-template <bool UseCurly=true>
-struct C {
-
-    struct complex_t {
-        float a;
-        float b;
-    };
-
-    /* If c_value was called value, then the primary template
-       my_traits could have been used if the to_string function
-       was defined. But std::to_string cannod be specialized for
-       user types in the namespace std. A using statement may solve
-       this but I would not judge that clean.
-       http://en.cppreference.com/w/cpp/language/extending_std
-    */
-
-    complex_t c_value;
-
-    C(float a, float b) : c_value{a,b} {}
-};
-
-/**
-   This is a specialization of the traits for the new class C
-   that comes from some other software.
- */
-template <bool UseCurl>
-struct my_traits<C<UseCurl>> {
-
-    static std::string left_separator() {return UseCurl?" {":" (";}
-    static std::string right_separator() {return UseCurl?"} ":") ";}
-    static std::string get_value(C<UseCurl> const& f)
-    {
-        return std::to_string(f.c_value.a) + " + i" + std::to_string(f.c_value.b);
-    }
-};
-
 void print( ) {
     std::cout << "." << std::endl;
 }
@@ -81,14 +83,47 @@ template <typename First, typename... T>
 void print(First const& f, T const&... v) {
     using traits = my_traits<First>;
 
-    std::cout << traits::left_separator() << traits::get_value(f) << traits::right_separator();
+    auto ls = print_impl::get_left_separator<traits>(f);
+    auto rs = print_impl::get_right_separator<traits>(f);
+    typename traits::value_type value = traits::get_value(f);
+
+    std::cout << ls << value << rs;
     print(v...);
+}
+
+
+struct complex { // A third party class
+    double re, im;
+
+    complex(double re, double im)
+        : re(re)
+        , im(im)
+    {}
+};
+
+template <>
+struct my_traits<complex> {
+
+    static constexpr bool has_separators = false;
+    using value_type = complex;
+
+
+    static value_type get_value(complex const& f)
+    {
+        return f;
+    }
+
+    static std::string default_separator() {return " $$ ";}
+};
+
+std::ostream& operator<<(std::ostream& s, complex const& c) {
+    return s << "{" << c.re << " + i" << c.im << "}";
 }
 
 
 int main() {
 
-    print(A<int>(2), B("ciao"), A<double>(3.1415926), B("this is it"), C<>(1.2, 3.4),  C<false>(1.2, 3.4) );
+    print(A<int>(2), B("ciao"), A<double>(3.1415926), B("this is it"), complex(1.41, 3.14));
 
     return 0;
 }

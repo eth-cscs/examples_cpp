adf012b4a99516b7170b26080498d998f82969c7
Mauro Bianco <mbianco@cscs.ch>
2016-07-01 17:57:54 +0200

    example of not generated move constructor because the copy is deleted

---
+++ Code/move_examples/move_constructor_automatic.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include "../show.h"
+
+struct movable {
+    int * pv = nullptr;
+    int s = 0;
+
+    movable() {}
+
+    movable(int s) : pv{new int[s]}, s{s} {std::cout << "Default\n";}
+
+
+    // The exixtence of a copy constructor prevent the automatic generation of the move constructor. Even if it's deleted.
+    movable(movable const & other) = delete;
+
+    int size() const {return s;}
+};
+
+int main() {
+    movable z(200);
+    SHOW(z.size()); // prints 200
+    movable u(std::move(z)); // move constructor
+    SHOW(z.size()); // prints 0
+    SHOW(u.size()); // prints 200
+}

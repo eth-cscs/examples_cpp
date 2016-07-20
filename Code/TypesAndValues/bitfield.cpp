bf961d64b19648e8b0ff522af9087f05467d2920
Mauro Bianco <mbianco@cscs.ch>
2016-07-20 14:26:46 +0200

    bit field using my instruction model

---
+++ Code/TypesAndValues/bitfield.cpp
@@ -0,0 +1,32 @@
+#include "../show.h"
+
+struct instr_t {
+    using basic_type = unsigned;
+    basic_type oper  : 3;
+    basic_type cond1 : 2;
+    basic_type val1  : 3;
+    basic_type cond2 : 2;
+    basic_type val2  : 3;
+    basic_type act   : 3;
+    basic_type cell  : 4;
+    basic_type : sizeof(basic_type)*8-21; // unused (not necessary at the pad)
+};
+
+std::ostream& operator<<(std::ostream& s, instr_t a) {
+    return s << "inst " << a.oper
+<< ", " << a.cond1
+                           << ", " << a.val1
+                                      << ", " << a.cond2
+                                                 << ", " << a.val2
+                                                            << ", " << a.act
+                                                                       << ", " << a.cell << "\n";
+}
+
+int main() {
+    SHOW(sizeof(instr_t));
+    instr_t x = instr_t{1,2,5,3,3,4,12};
+    for (int i=0; i<10; ++i) {
+        SHOW(x);
+        x.cond1 += 1;
+    }
+}

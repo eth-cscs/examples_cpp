184a5d51346a38388b8ad8c724d1ce56d7f0fe9c
Mauro Bianco <mbianco@cscs.ch>
2016-07-20 14:46:55 +0200

    added destructor

--- Code/move_examples/move_only_fail.cpp
+++ Code/move_examples/move_only_fail.cpp
@@ -8,13 +8,14 @@ struct movable {
 
     movable(int s) : v(s) {std::cout << "Default\n";}
     //movable(movable&& other) = delete; //: v(std::move(other.v)) {std::cout << "Move\n";}
-    //movable(movable const & other) = delete; //: v(other.v) {std::cout << "Copy\n";}
+    //movable(movable const & other) : v(other.v) {std::cout << "Copy\n";}
+    //~movable() {}
 };
 
 int main() {
     movable z(movable(200));
     SHOW(z.v.size());
-    // movable u(std::move(z));
-    // SHOW(z.v.size());
-    // SHOW(u.v.size());
+    movable u(std::move(z));
+    SHOW(z.v.size());
+    SHOW(u.v.size());
 }

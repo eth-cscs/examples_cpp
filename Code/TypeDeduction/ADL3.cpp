b99254609ebc6d9692df8cd5801c3c977b26a02a
Mauro Bianco <mbianco@cscs.ch>
2016-07-21 16:48:01 +0200

    example to show that mental model for template substitution as text substitution is not always valid. Plus an example explicitly showing the order of template arguments in function templates

---
+++ Code/TypeDeduction/ADL3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// The mental model of templates that substitute text is ok in almost all cases.
+// But in this example we can see that ADL does not apply in template instantiation!
+
+namespace name {
+    struct X {};
+    void foo(X) { std::cout << "name::foo: ";}
+}
+
+void foo(name::X) { std::cout << "::foo: ";}
+
+
+template <typename T, void (*F)(T)>
+void bar(T x) {
+    F(x);
+    //foo(x); // This goes int ADL
+    std::cout << "ADL does not apply\n";
+};
+
+int main() {
+    //foo(name::X{}); // Regular ADL
+
+    bar<name::X, foo>(name::X{}); // bar calls foo(name::X) but it works! No ADL is used
+    //bar<name::X, name::foo>(name::X{});
+}

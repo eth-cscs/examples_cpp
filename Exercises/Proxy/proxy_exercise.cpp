659010867863968b8ed6d86ae6fa0b4f4b317627
Mauro Bianco <mbianco@cscs.ch>
2016-08-10 09:47:01 +0200

    added a proxy exercize based on the proxy iterator

--- Exercises/Proxy/proxy_exercise.cpp
+++ Exercises/Proxy/proxy_exercise.cpp
@@ -6,33 +6,19 @@
 template <typename It0, typename It1>
 struct zip_proxy {
 
-    std::pair<It0, It1> m_zip;
+    zip_proxy(It0 it0, It1 it1);
 
+    std::pair<const It0, const It1> operator*() const;
 
-    zip_proxy(It0 it0, It1 it1)
-        : m_zip(it0,it1)
-    {}
+    std::pair<It0, It1>& operator*();
 
-    std::pair<const It0, const It1> operator*() const {
-        return m_zip;
-    }
-
-    std::pair<It0, It1>& operator*() {
-        return m_zip;
-    }
-
-    zip_proxy& operator++() {
-        ++(m_zip.first);
-        ++(m_zip.second);
-        return *this;
-    }
-
+    zip_proxy& operator++();
 };
 
 
 template <typename It0, typename It1>
 bool operator!=(zip_proxy<It0,It1> const& i0, zip_proxy<It0, It1> const& i1) {
-    return ((*i0).first != (*i1).first) and ((*i0).second != (*i1).second);
+    return /*???*/ ;
 }
 
 

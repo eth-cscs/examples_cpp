5e8f2001b1d6c6e1555621ab303f0e5008df1e40
Mauro Bianco <mbianco@cscs.ch>
2016-08-22 09:41:38 +0200

    more on type deduction

--- Code/TypeDeduction/auto.cpp
+++ Code/TypeDeduction/auto.cpp
@@ -1,10 +1,14 @@
 #include "../show.h"
+#include <tuple>
+#include <string>
 
 struct A {
     int &x;
     A(int &x) : x(x) {}
 
     int& get() { return x;}
+
+    float value() const {return x;};
 };
 
 struct B {
@@ -12,6 +16,8 @@ struct B {
     B(int x) : x(x) {}
 
     int get() { return x;}
+
+    char value() const {return x;};
 };
 
 void test() {
@@ -44,6 +50,41 @@ void test() {
     }
 }
 
+
+template <typename F>
+auto foo(F x)
+    -> decltype(x.get()) // only C++11
+{
+    return x.get();
+}
+
+template <typename F>
+decltype(auto) bar(F x) {
+    return x.get();
+}
+
+
 int main() {
     test();
+
+    std::tuple<int, float, char, std::string> t1{42, 3.14, 'z', std::string{"hello"}};
+    auto t2 = std::make_tuple(42, 3.14f, 'z', std::string{"hello"});
+
+    int x = 10;
+
+    SHOW(inc(A(x)));
+    //SHOW(inc(B(x)));
+
+    SHOW(foo(A(x)));
+    SHOW(foo(B(x)));
+
+    SHOW(bar(A(x)));
+    decltype(auto) x1 = bar(A(x));
+    x1++;
+    SHOW(bar(A(x)));
+
+    SHOW(bar(B(x)));
+    decltype(auto) x2 = bar(B(x));
+    x2++;
+    SHOW(bar(B(x)));
 }
--- Code/TypeDeduction/typededuction.cpp
+++ Code/TypeDeduction/typededuction.cpp
@@ -2,6 +2,18 @@
 
 template <typename T0, typename T1, typename T2>
 void
+check_these(T0 x, T1 y, T2 z) {
+    static_assert(std::is_same<T0, int>::value, "1");
+    static_assert(std::is_same<T1, int>::value, "2");
+    static_assert(std::is_same<T2, int>::value, "3");
+
+    static_assert(std::is_same<decltype(x), int>::value, "1");
+    static_assert(std::is_same<decltype(y), int>::value, "1");
+    static_assert(std::is_same<decltype(z), int>::value, "1"); // !!!!
+}
+
+template <typename T0, typename T1, typename T2>
+void
 check_these_cr(T0 const& x, T1 const& y, T2 const& z) {
     static_assert(std::is_same<T0, int>::value, "1");
     static_assert(std::is_same<T1, int>::value, "2");
@@ -11,7 +23,7 @@ check_these_cr(T0 const& x, T1 const& y, T2 const& z) {
     static_assert(std::is_same<decltype(y), int const&>::value, "1");
     static_assert(std::is_same<decltype(z), int const&>::value, "1"); // !!!!
 
-    #if __cplusplus >= 201402L
+#if __cplusplus >= 201402L
     decltype(auto) _x = x;
     static_assert(std::is_same<decltype(_x), const int&>::value, "1");
     decltype(auto) _y = y;
@@ -71,12 +83,11 @@ check_these_rvr(T0 && x, T1 && y, T2 && z) {
 #endif
 }
 
-
-
 int main() {
     int x = 10;
     int const& y = x;
 
+    check_these(x, y, 10);
     check_these_cr(x, y, 10);
     check_these_rvr(x, y, 10);
 

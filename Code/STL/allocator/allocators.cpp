2ea99c835486fb9365728b8d75247c8b1fbce2b0
Paolo Crosetto <paolo.crosetto@usys.ethz.ch>
2016-09-13 15:55:52 +0000

    update

--- Code/STL/allocator/allocators.cpp
+++ Code/STL/allocator/allocators.cpp
@@ -6,29 +6,12 @@
 template<typename T>
 struct my_heap_allocator{
     using value_type=T;
-    using propagate_on_container_move_assignment = std::true_type;
-    using is_always_equal = std::true_type;
 
     T* allocate (int n, std::allocator<void>::const_pointer hint=0){
       if(n<100)  return static_cast<T*>(malloc(n*sizeof(T)));
       else throw(std::bad_alloc());
     }
     void deallocate (T* p, int n){free(p);}
-};
-
-
-template<typename T>
-struct my_stack_allocator{
-    T data[100];
-    using value_type=T;
-    using propagate_on_container_move_assignment = std::true_type;
-    using is_always_equal = std::true_type;
-
-    T* allocate (int n, std::allocator<void>::const_pointer hint=0){
-      if(n<100)  return data;
-      else throw(std::bad_alloc());
-    }
-    void deallocate (T* p, int n){/*RAII*/}
 };
 
 main(){

e594f252e7936d7867fca5e43ad0d9a962a356c2
Paolo Crosetto <paolo.crosetto@usys.ethz.ch>
2016-09-21 17:46:14 +0000

    updating tuple example

--- Code/STL/tuple/my_tuple.cpp
+++ Code/STL/tuple/my_tuple.cpp
@@ -3,7 +3,7 @@ template<typename ... T> struct my_tuple;
 template<typename First, typename ... T>
 struct my_tuple<First, T...> : public my_tuple<T ...> {
     using super=my_tuple<T ...>;
-    static constexpr int size = sizeof...(T)+1;
+    static constexpr int size = sizeof...(T);
 
     constexpr my_tuple(First first_, T ... rest_) : super(rest_ ...), m_value(first_){}
 

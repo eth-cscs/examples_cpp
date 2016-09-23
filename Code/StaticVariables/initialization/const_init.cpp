7e10fd4c8be65ef7588c327dd717dcbbe54e163c
Paolo Crosetto <paolo.crosetto@usys.ethz.ch>
2016-09-23 13:02:45 +0000

    updating the static_init examples

--- Code/StaticVariables/initialization/const_init.cpp
+++ Code/StaticVariables/initialization/const_init.cpp
@@ -1,29 +1,17 @@
 #include <iostream>
-
-struct F{
-    static int m_f ;
-};
-
-struct D{
-    static int m_d ;
-};
-
 struct S {
-    static int c;
+    static constexpr int s_c = 5;
 };
-
-int F::m_f = 10 * D::m_d ;
-
-int S::c = 5;
-
-int D::m_d = 10 * S::c;
-
-
-int main()
-{
-    std::cout << "F::m_f = " << F::m_f << '\n';
-    std::cout << "D::m_d::value = " << D::m_d << '\n';
-    std::cout << "S::c::value = " << S::c << '\n';
-    //std::array<int, S::c> a1; // OK: S::c is a constant expression
-//  std::array<int, d> a2;    // error: d is not a constant expression
-}
+struct D{
+    static constexpr int s_d = 10 * S::s_c ;
+};
+struct F{
+    static constexpr int s_f = 10 * D::s_d ;
+};
+constexpr int F::s_f;
+constexpr int D::s_d;
+constexpr int S::s_c;
+int main(){
+    std::cout << "s_f = " << F::s_f << '\n'; // 500
+    std::cout << "s_d = " << D::s_d << '\n'; // 50
+    std::cout << "s_c = " << S::s_c << '\n';}// 5

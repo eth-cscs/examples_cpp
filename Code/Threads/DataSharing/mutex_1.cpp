061379da61dee50550f7040701934062f68aad89
bignamic <bignamini@cscs.ch>
2016-09-25 21:45:04 +0200

    New mutex example created

--- Code/Threads/DataSharing/mutex_1.cpp
+++ Code/Threads/DataSharing/mutex_1.cpp
@@ -1,113 +1,74 @@
 #include <thread>
 #include <mutex>
 #include <iostream>
-#include <list>
 #include <cstdlib>
 
-std::list<int> elements;
 std::mutex mutex;
 
-void add(unsigned int i_num_elements){
-
-
-  for(unsigned int i=0;i<i_num_elements;++i)
-    elements.push_back(0);
-
-  return;
-
+void update_counter(unsigned int& io_counter) {
+	io_counter++;
 }
 
-void protected_add(unsigned int i_num_elements){
+void protected_update_counter(unsigned int& io_counter) {
 
-  //  std::mutex mutex;// What if I decomment this line?
-
-  mutex.lock();
-
-  for(unsigned int i=0;i<i_num_elements;++i)
-    elements.push_back(0);
-
-  mutex.unlock();
-
-  return;
-
+//	std::mutex mutex;// What if I decomment this line?
+	mutex.lock();	
+	io_counter++;
+	mutex.unlock();
 }
 
+void protected_guard_update_counter(unsigned int& io_counter) {
 
-void protected_guard_add(unsigned int i_num_elements){
-
-  std::mutex mutex;// What if I decomment this line?                                                                                                                                                                                                                                                                      
-  std::lock_guard<std::mutex> guard(mutex);
-  for(unsigned int i=0;i<i_num_elements;++i)
-    elements.push_back(0);
-
-
-  return;
-
-}
-
-void protected_guard_add_ref(unsigned int i_num_elements, std::list<int>& io_elements){
-
-  
-  // std::lock_guard<std::mutex> guard(mutex);
-  for(unsigned int i=0;i<i_num_elements;++i)
-    io_elements.push_back(0);  
- 
-  return;
-
+	//  std::mutex mutex;// What if I decomment this line?
+	std::lock_guard<std::mutex> guard(mutex);
+	io_counter++;
 }
 
 int main()
 {
 
-  //  srand (time(NULL));
+	unsigned int counter=0;
+	// 1 - Non protected update
+	std::cout<<"Non protected update"<<std::endl;
+	for(unsigned int i=0;i<100000;i++){
+		std::thread t1(update_counter,std::ref(counter));
+		std::thread t2(update_counter,std::ref(counter));
+		update_counter(counter);
+		t1.join();
+		t2.join();
+		if(counter!=3)			
+			std::cout<<"Counter: "<<counter<<std::endl;
+		counter=0;
+	}
 
-  unsigned int num_elements_1 = rand() % 1000;
-  unsigned int num_elements_2 = rand() % 1000;
+	
+	// 2 - Protected update (with "manual" mutex lock/unlock)
+	std::cout<<"Protected update (with manual mutex lock/unlock)"<<std::endl;
+	for(unsigned int i=0;i<100000;i++){
+		std::thread t1(protected_update_counter,std::ref(counter));
+		std::thread t2(protected_update_counter,std::ref(counter));
+		protected_update_counter(counter);
+		t1.join();
+		t2.join();
+		if(counter!=3)
+			std::cout<<"Counter: "<<counter<<std::endl;
+		counter=0;
+	}
 
-  //// Non protected element add case
-  std::thread t1(add, num_elements_1);
-  std::thread t2(add, num_elements_2);
- 
-  t1.join();
-  t2.join();
-  
-  std::cout<<"Num elements (non protected case): "<<elements.size()<<" vs "<<num_elements_1+num_elements_2<<std::endl;
-  ////
-
-  //// Protected element add case (lock/unlock)
-  elements.clear();
-  
-  t1 = std::thread(protected_add, num_elements_1);
-  t2 = std::thread(protected_add, num_elements_2);
-
-  t1.join();
-  t2.join();
-
-  std::cout<<"Num elements (protected case): "<<elements.size()<<" vs "<<num_elements_1+num_elements_2<<std::endl;
-  ////
-
-  //// Protected element add case (with lock guard)
-  elements.clear();
-
-  t1 = std::thread(protected_guard_add, num_elements_1);
-  t2 = std::thread(protected_guard_add, num_elements_2);
-
-  t1.join();
-  t2.join();
-
-  std::cout<<"Num elements (protected guard case): "<<elements.size()<<" vs "<<num_elements_1+num_elements_2<<std::endl;
-  ////
-
-  //// Protected element add case referencing a non global variable (with lock guard)
-  std::list<int> local_elements;
-
-  t1 = std::thread(protected_guard_add_ref, num_elements_1, std::ref(local_elements));
-  t2 = std::thread(protected_guard_add_ref, num_elements_2, std::ref(local_elements));
-
-  t1.join();
-  t2.join();
-
-  std::cout<<"Num elements (protected guard case/ref): "<<local_elements.size()<<" vs "<<num_elements_1+num_elements_2<<std::endl;
-  ////
+	// 3 - Protected update (with lock_guard)
+	std::cout<<"Protected update (with lock_guard)"<<std::endl;
+	for(unsigned int i=0;i<100000;i++){
+		std::thread t1(protected_guard_update_counter,std::ref(counter));
+		std::thread t2(protected_guard_update_counter,std::ref(counter));
+		protected_guard_update_counter(counter);
+		t1.join();
+		t2.join();
+		if(counter!=3)
+			std::cout<<"Counter: "<<counter<<std::endl;
+		counter=0;
+	}
+	
+	
+	return 0 ;
 
 }

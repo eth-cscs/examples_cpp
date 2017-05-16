97b8de358f5e689004400419a5f885936b930494
Mauro Bianco <mbianco@cscs.ch>
2017-05-16 20:52:36 +0200

    comments

--- Code/TemplateMetaProgramming/merge_sort.cpp
+++ Code/TemplateMetaProgramming/merge_sort.cpp
@@ -2,13 +2,18 @@
 
 using uint = unsigned int;
 
+/**
+   A compile time sequence of values of type T
+ */
 template <typename T, T ...Vs>
 struct s_vector {
     using value_type = T;
     static constexpr uint size = sizeof...(Vs);
 };
 
-
+/**
+   Metafunction to concatenate to s_vectors
+ */
 template <typename S1, typename S2>
 struct concat;
 
@@ -17,6 +22,11 @@ struct concat<s_vector<T, s0...>, s_vector<T, s1...>> {
     using type = s_vector<T, s0..., s1...>;
 };
 
+/**
+   Function to print recursively the content fo an s_vector,
+   but actually it would work for any variadic pack of
+   values of the same type
+ */
 template <typename T>
 void out(std::ostream& s, T F) {
     s << F;
@@ -28,6 +38,9 @@ void out(std::ostream& s, T F, Ts... Vs) {
     out(s, Vs...);
 }
 
+/**
+   Function to print an s_vector
+ */
 template <typename T, T ...Vs>
 std::ostream& operator<<(std::ostream& s, s_vector<T, Vs...>) {
     s << "s_vector<";
@@ -37,6 +50,12 @@ std::ostream& operator<<(std::ostream& s, s_vector<T, Vs...>) {
 }
 
 
+/**
+   Meta-function to access an element of an s_vector.
+   This is far more general, it works with any container
+   that has takes the type and the sequence of values
+   as variadic pack, plus a ::size member.
+ */
 template <typename Seq, uint Index>
 struct at {
     static_assert(Seq::size > Index, "Out of bound");
@@ -65,13 +84,19 @@ struct at {
 };
 
 
-
+/**
+   Meta-function to get an s_vector that is a sorted version of the
+   input s_vector. It would not be difficult to generalize it to get a
+   sequence which is an instantiation of the Input template, but it's
+   already complicated enough.
+ */
 template <typename Input>
 struct merge_sort {
 
-    template <typename S0, typename S1>
-    struct merge;
-
+    /**
+       Meta-function to perform a selecion of type based on a boolean
+       value
+     */
     template <bool TakeFirst, typename S0, typename>
     struct pickfirst {
         using type = S0;
@@ -82,6 +107,19 @@ struct merge_sort {
         using type = S1;
     };
 
+    /**
+       Meta-function to perform the merge of two sorted sequences.
+    */
+    template <typename S0, typename S1>
+    struct merge;
+
+    /**
+       Meta-function to perform the merge of two sorted sequences.
+
+       This is the case in which both sequences are not empty. We then
+       take the minimum of the fron values and concatentate with the
+       merge of the rest.
+     */
     template <template <typename T, T... > class Seq,
               typename Input::value_type F0,
               typename Input::value_type ...V0s,
@@ -101,6 +139,12 @@ struct merge_sort {
             >::type;
     };
 
+
+    /**
+       Meta-function to perform the merge of two sorted sequences.
+
+       Speciaization for when the first sequence is empty
+     */
     template <template <typename T, T... > class Seq,
               typename Input::value_type ...V1s>
               struct merge<Seq<typename Input::value_type >,
@@ -109,6 +153,11 @@ struct merge_sort {
     };
 
 
+    /**
+       Meta-function to perform the merge of two sorted sequences.
+
+       Speciaization for when the second sequence is empty
+     */
     template <template <typename T, T... > class Seq,
               typename Input::value_type ...V0s>
     struct merge<Seq<typename Input::value_type, V0s... >,
@@ -117,7 +166,9 @@ struct merge_sort {
     };
 
 
-
+    /**
+       Recursive splitting and merging
+     */
     template <typename In, uint Low, uint Hi, typename Void=void>
     struct sort_chunk {
         static const uint Split = (Hi+Low+1) >> 1;
@@ -125,6 +176,10 @@ struct merge_sort {
                                      typename sort_chunk<In, Split+1, Hi>::type>::type;
     };
 
+    /**
+       Recursive splitting and merging. Case of the size two in which
+       there is the comare&swap
+     */
     template <typename In, uint Low, uint Hi>
     struct sort_chunk<In, Low, Hi, typename std::enable_if< Hi-Low==1 >::type > {
         template <bool b, typename In::value_type l, typename In::value_type h>
@@ -145,11 +200,15 @@ struct merge_sort {
         using type = typename swap_if< (L>H), L, H>::type;
     };
 
+    /**
+       Recursive splitting and merging. Case of a size one sequence.
+     */
     template <typename In, uint Low, uint Hi>
     struct sort_chunk<In, Low, Hi, typename std::enable_if< (Hi-Low==0) >::type > {
         using type = s_vector<typename In::value_type, at<In, Low>::value >;
     };
 
+    /** Start the sorting */
     using type = typename sort_chunk<Input, 0, Input::size-1>::type;
 
 };

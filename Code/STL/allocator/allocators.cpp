e97c1b4a1abc29b56c1eeec5a6dc32d424ebd695
Mauro Bianco <mbianco@cscs.ch>
2017-08-10 15:25:28 +0200

    reworked another couple of examples

--- Code/STL/allocator/allocators.cpp
+++ Code/STL/allocator/allocators.cpp
@@ -4,11 +4,41 @@
 #include<cassert>
 #include<iostream>
 
-//#include "stack.cpp"
-#include "heap.cpp"
+#include "stack.hpp"
+#include "heap.hpp"
 
-main(){
+void heap() {
+    std::cout << " >> heap allocator <<\n";
+
     std::vector<int,  my_heap_allocator<int> > q_({0,1,2,3,4,5,6,7});
     assert(q_[2]==2);
-    //std::vector<int,  my_heap_allocator<int> > q_(101); //throws bad_alloc
+
+    try {
+        std::vector<int,  my_heap_allocator<int> > q_(101); //throws bad_alloc
+    } catch (std::bad_alloc const&) {
+        std::cout << "Only 100 elements are allowed\n";
+    }
+}
+
+void stack() {
+    std::cout << " >> stack allocator <<\n";
+
+    std::vector<int,  my_stack_allocator<int> > q_({0,1,2,3,4,5,6,7});
+    assert(q_[2]==2);
+
+    try {
+        std::vector<int,  my_stack_allocator<int> > q_(101); //throws bad_alloc
+    } catch (std::bad_alloc const&) {
+        std::cout << "Only 100 elements are allowed\n";
+    }
+}
+
+
+int main() {
+
+    std::cout << " > allocators <\n";
+
+    heap();
+
+    stack();
 }

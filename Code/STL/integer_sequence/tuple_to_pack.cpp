0ed266848940c1c3684b521232e10b786efca0f5
Mauro Bianco <mbianco@cscs.ch>
2017-09-12 15:25:22 +0200

    more intuitive example for tuple_to_pack

--- Code/STL/integer_sequence/tuple_to_pack.cpp
+++ Code/STL/integer_sequence/tuple_to_pack.cpp
@@ -1,14 +1,34 @@
 #include <tuple>
 #include <array>
+#include <iostream>
+
+template <typename Only>
+void print(Only const& o) {
+    std::cout << o;
+}
+
+template <typename First, typename ...Ts>
+void print(First const& f, Ts const &...v) {
+    std::cout << f << ", ";
+    print(v...);
+}
 
 template<typename Tuple, int ... Ids>
-std::array<int, sizeof...(Ids)>
-create_vector(Tuple& tuple_,
-              std::integer_sequence<int, Ids ...>){
-  return {sizeof(std::get<Ids>(tuple_)) ...};
+void
+unpack(Tuple& tuple_,
+       std::integer_sequence<int, Ids...>){
+    print(std::get<Ids>(tuple_)...);
+
+}
+
+template <typename ...Vs>
+auto print_tuple(std::tuple<Vs...> const & x) {
+    std::cout << "Tuple (";
+    unpack(x, std::make_integer_sequence<int, sizeof...(Vs)>{});
+    std::cout << ")\n";
 }
 
 int main(){
     auto tuple_ = std::make_tuple(1., 'b', 2u, "ciao");
-    auto arr = create_vector(tuple_, std::make_integer_sequence<int, 4>());
+    print_tuple(tuple_);
 }

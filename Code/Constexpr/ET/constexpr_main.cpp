6ab82d1c8e42b25d04dbc02a9e6afb54595eb707
Mauro Bianco <mbianco@cscs.ch>
2017-09-12 23:22:36 +0200

    removed old version

--- Code/Constexpr/ET/constexpr_main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(int argc , char** argv){
-
-    constexpr auto expr = x*x+x;
-
-    static_assert(expr(5.) == 30);
-}
--- Code/Constexpr/ET/p.cpp
+++
@@ -1,7 +0,0 @@
-struct p{
-    constexpr p(){}
-    template <typename T>
-    constexpr T operator()(T t_) const
-    {return t_;}
-};
-constexpr p x = p();

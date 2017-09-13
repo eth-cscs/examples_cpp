09c534d43ae1a995d2534a85cffae83679f25e27
Mauro Bianco <mbianco@cscs.ch>
2017-09-13 21:10:25 +0200

    automatic differentiation in C++11 - checkout the differences in this case

---
+++ Code/Constexpr/ET/expr_times_cxx11.hpp
@@ -0,0 +1,47 @@
+namespace expressions {
+
+template <typename T1, typename T2> struct expr_times {
+  T1 arg1;
+  T2 arg2;
+
+  constexpr expr_times(T1 arg1, T2 arg2) : arg1(arg1), arg2(arg2) {}
+
+    template <typename T> constexpr auto operator()(T t_) const -> decltype(arg1(t_) * arg2(t_)) {
+    return arg1(t_) * arg2(t_);
+  }
+
+  std::string to_string() const {
+    return std::string("(") + arg1.to_string() + " * " + arg2.to_string() +
+           std::string(")");
+  }
+
+  constexpr int sum_ops() const { return arg1.sum_ops() + arg2.sum_ops(); }
+  constexpr int mult_ops() const {
+    return arg1.mult_ops() + arg2.mult_ops() + 1;
+  }
+};
+
+// This specialization is not useful for the functionality, but to
+// reduce the length of the output expression
+template <typename T, typename X> struct expr_times<c<T>, c<X>> {
+  c<T> value;
+
+  constexpr expr_times(c<T> arg1, c<X> arg2) : value{arg1.value * arg2.value} {}
+
+  template <typename U> constexpr T operator()(U) const {
+    return value.value;
+    ;
+  }
+
+  std::string to_string() const { return value.to_string(); }
+
+  constexpr int sum_ops() const { return 0; }
+  constexpr int mult_ops() const { return 0; }
+};
+
+/** sum expression*/
+template <typename T1, typename T2>
+constexpr expr_times<T1, T2> operator*(T1 arg1, T2 arg2) {
+  return expr_times<T1, T2>(arg1, arg2);
+}
+} // namespace expressions
--- Code/Constexpr/ET/expressions.hpp
+++ Code/Constexpr/ET/expressions.hpp
@@ -9,7 +9,15 @@ template <typename, typename> struct expr_plus;
 template <typename, typename> struct expr_times;
 
 #include "constant.hpp"
+#include "p.hpp"
+
+#if __cplusplus >= 201402L
 #include "expr_plus.hpp"
 #include "expr_times.hpp"
-#include "p.hpp"
 #include "expr_derivative.hpp"
+#else
+#pragma message("Compiling with C++11")
+#include "expr_plus_cxx11.hpp"
+#include "expr_times_cxx11.hpp"
+#include "expr_derivative_cxx11.hpp"
+#endif

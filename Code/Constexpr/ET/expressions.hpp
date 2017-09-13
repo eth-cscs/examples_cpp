a48014d8e5f347a35600df0537bdaf4de585d862
Mauro Bianco <mbianco@cscs.ch>
2017-09-13 14:32:14 +0200

    clang format and fix

--- Code/Constexpr/ET/expressions.hpp
+++ Code/Constexpr/ET/expressions.hpp
@@ -9,7 +9,7 @@ template <typename, typename> struct expr_plus;
 template <typename, typename> struct expr_times;
 
 #include "constant.hpp"
-#include "expr_derivative.hpp"
 #include "expr_plus.hpp"
 #include "expr_times.hpp"
 #include "p.hpp"
+#include "expr_derivative.hpp"

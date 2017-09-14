148bf07bf59c3db093de1817ec0e07cce8c7e5a4
Mauro Bianco <mbianco@cscs.ch>
2017-09-14 09:12:38 +0200

    wrong include order

--- Code/Constexpr/ET/expressions.hpp
+++ Code/Constexpr/ET/expressions.hpp
@@ -12,12 +12,12 @@ template <typename, typename> struct expr_times;
 #include "p.hpp"
 
 #if __cplusplus >= 201402L
-#include "expr_derivative.hpp"
 #include "expr_plus.hpp"
 #include "expr_times.hpp"
+#include "expr_derivative.hpp"
 #else
 #pragma message("Compiling with C++11")
-#include "expr_derivative_cxx11.hpp"
 #include "expr_plus_cxx11.hpp"
 #include "expr_times_cxx11.hpp"
+#include "expr_derivative_cxx11.hpp"
 #endif

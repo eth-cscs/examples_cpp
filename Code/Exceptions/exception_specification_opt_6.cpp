b472fb4a96ce1b8d9c7c4cb13d8e39d5a4fa57c8
Christopher Bignamini <christopher.bignamini@gmail.com>
2017-09-26 16:29:13 +0200

    exception_specification_opt_6.cpp
    First exception handling example created (noexcept-move performance)

---
+++ Code/Exceptions/exception_specification_opt_6.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+
+#define PRINT_OPERATION
+
+struct MyObject {
+
+  MyObject(void)
+  {
+  }
+  
+  MyObject(int i_size)
+  {
+    m_data.resize(i_size);
+  }
+
+
+  MyObject(const MyObject& i_object)
+    :m_data(i_object.m_data)
+  {
+#ifdef PRINT_OPERATION
+    std::cout<<"cp ctor"<<std::endl;
+#endif
+  }
+  
+  // 3. Try to add/remove the noexcept specifier
+  MyObject(MyObject&& i_object) //noexcept
+    :m_data(std::move(i_object.m_data))
+  {
+#ifdef PRINT_OPERATION
+    std::cout<<"mv ctor"<<std::endl;
+#endif
+  }
+
+
+  MyObject& operator=(const MyObject& i_object)
+  {
+#ifdef PRINT_OPERATION
+    std::cout<<"cp ="<<std::endl;
+#endif
+    m_data = i_object.m_data;
+    return *this;
+  } 
+
+  MyObject& operator=(MyObject&& i_object) noexcept
+  {
+#ifdef PRINT_OPERATION
+    std::cout<<"mv ="<<std::endl;
+#endif
+    m_data = std::move(i_object.m_data);
+    return *this;
+  }
+
+  std::vector<int> m_data;
+
+};
+
+
+int main() {
+
+  // 1. Create a std::vector of MyObject
+  std::vector<MyObject> obj_va(10,MyObject(1000000));
+
+  // 2. Resize (new memory location) 
+  std::clock_t start;
+  double duration;
+  start = std::clock();  
+  obj_va.resize(3000);
+  duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+
+
+  std::cout<<"Resize duration "<<duration<<std::endl;
+    
+}

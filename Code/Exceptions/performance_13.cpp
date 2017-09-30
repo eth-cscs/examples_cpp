f4e857763ba10963c9d6278f7b51d6db71f33e1f
bignamic <bignamini@cscs.ch>
2017-09-30 10:45:38 +0200

    Include list fix for gcc

--- Code/Exceptions/performance_13.cpp
+++ Code/Exceptions/performance_13.cpp
@@ -1,32 +1,33 @@
 #include <exception>
 #include <ctime>
 #include <iostream>
+#include <cstdlib>
 #include "performance_fun.hpp"
 
 
 int main(){
 
   int N_IT;
-  std::cout<<"Enter number of loop iterations (I would suggest 1000000...): ";
+  std::cout<<"Enter number of loop iterations (I would suggest 10000000...): ";
   std::cin>>N_IT;
   if(N_IT<0){
     std::cout<<"Number of iteration must be non negative"<<std::endl;
     std::terminate();
   }
-    
-  srand(23);
+
+  std::srand(23);
   std::clock_t start;
   double duration;
   int rand_int;
 
   int error_status = 0;
-  start = std::clock();  
+  start = std::clock();
   for(unsigned int i=0;i<N_IT;i++){
-    
-    rand_int=rand()%2;
+
+    rand_int=std::rand()%2;
     error_status = test_fun(rand_int);
     if(error_status){
-      error_status = 2;
+      error_status = 0;
     }
 
   }
@@ -34,15 +35,15 @@ int main(){
   std::cout<<"Loop duration (error status): "<<duration<<std::endl;
 
 
-  start = std::clock();  
+  start = std::clock();
   for(unsigned int i=0;i<N_IT;i++){
-    
-    rand_int=rand()%2;
+
+    rand_int=std::rand()%2;
     try {
       test_fun_exc(rand_int);
     }
     catch(std::exception) {
-      error_status = 2;
+      error_status = 0;
     }
   }
 

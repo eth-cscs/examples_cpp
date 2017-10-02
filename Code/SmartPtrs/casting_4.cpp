0a65b84c365af803bf411ce5e885a5ff6a81a5e9
bignamic <bignamini@cscs.ch>
2017-10-02 15:29:18 +0200

    Bugs fixed

--- Code/SmartPtrs/casting_4.cpp
+++ Code/SmartPtrs/casting_4.cpp
@@ -3,25 +3,21 @@
 
 struct Base{
 
-	virtual bool f(void) const { return false; }	
+	virtual bool f(void) const { return false; }
 };
 
 
 struct Derived : public Base {
-	
+
 	bool f(void) const { return true; }
 
 	void setter(void) {}
-		
+
 };
 
+void f(std::shared_ptr<const Derived> i_derived) {
 
-bool f(std::shared_ptr<const Base> i_sp_base) {
-
-	// 7 - Dynamic downcast to a shared_ptr to Derived object type 
-	std::shared_ptr<const Derived> sp_derived_cast_from_base = std::static_pointer_cast<const Derived>(i_sp_base);
-	return sp_derived_cast_from_base->f();
-		
+    std::const_pointer_cast<Derived>(i_derived)->setter();
 }
 
 int main(){
@@ -29,27 +25,31 @@ int main(){
 	// 0 - Create shared_ptr to Base object type
 	std::shared_ptr<Base> sp_base(new Base);
 
-	// 1 - Static downcast to a shared_ptr to Derived object type
-	std::shared_ptr<Derived> sp_cast_from_base = std::static_pointer_cast<Derived>(sp_base);
+    // 1 - Create shared_ptr to Derived object type
+    std::shared_ptr<Derived> sp_derived(new Derived);
 
-	// 2 - Which method are we calling?
-    assert(sp_cast_from_base->f()==false);
+    // 2 - Static upcast of sp_derived to a new shared_ptr to base type
+    std::shared_ptr<Base> sp_base_from_derived = sp_derived;
 
 	// 3 - Check smart pointer count
-	assert(sp_base.use_count() == 2);
-	assert(sp_cast_from_base.use_count()==2);
+    assert(sp_derived.use_count() == 2);
 
-	// 4 - Dynamic downcast to a shared_ptr to Derived object type
-	std::shared_ptr<Derived> sp_dyna_cast_from_base = std::dynamic_pointer_cast<Derived>(sp_base);
+	// 4 - Static downcast of sp_base_from_derived to a shared_ptr to Derived object type
+	std::shared_ptr<Derived> sp_derived_from_base = std::static_pointer_cast<Derived>(sp_base_from_derived);
 
-	// 5 - Check smart pointer validity
-	assert(sp_dyna_cast_from_base==nullptr);
+	// 5 - Check smart pointer count
+    assert(sp_derived.use_count() == 3);
 
-	// 6 - Call a function waiting for a shared_ptr to Base passing a shared_ptr to Derived
-	assert(f(std::shared_ptr<const Derived>(new Derived)));
+	// 6 - Which method are we calling?
+    assert(sp_base_from_derived->f()==true);
 
-	// 8 - Complete my code and call the non const method on sp_const_derived
-	std::shared_ptr<const Derived> sp_const_derived(new Derived);
-	std::const_pointer_cast<Derived>(sp_const_derived)->setter();
-	
+	// 7 - Dynamic downcast of sp_base to a shared_ptr to Derived object type
+	std::shared_ptr<Derived> sp_derived_dyna_cast_from_base = std::dynamic_pointer_cast<Derived>(sp_base);
+
+	// 8 - Check smart pointer validity
+	assert(sp_derived_dyna_cast_from_base==nullptr);
+
+	// 9 - Complete my code and call the non const method on sp_const_derived
+    f(sp_derived);
+
 }

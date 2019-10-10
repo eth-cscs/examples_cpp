823a45b752e799509642d2c9f9f5c54cef501f6a
Mauro Bianco <mbianco@cscs.ch>
2019-10-10 14:32:48 +0200

    comment on compiler explorer output

--- Code/TypesAndValues/BrokenABI/broken.cpp
+++ Code/TypesAndValues/BrokenABI/broken.cpp
@@ -15,3 +15,18 @@ int main() {
     foo(a,b,g,p);
 }
 
+/*
+ COMPILER EXPLORER
+    EXPLICIT CONSTRUCTOR
+        mov     rax, QWORD PTR [rbp-32]
+        mov     QWORD PTR [rbp-24], rax
+        lea     rax, [rbp-24]
+        mov     rdi, rax
+        call    f(pair<int, int>)
+        lea     rax, [rbp-24]
+        mov     rdi, rax
+        call    pair<int, int>::~pair() [complete object destructor]
+    DEFAULT CONSTRUCTOR
+        mov     rax, QWORD PTR [rbp-8]
+        mov     rdi, rax
+        call    f(pair<int, int>)
